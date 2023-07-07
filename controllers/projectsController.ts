108409c99ddd3f26c6f023f984b339d09385ad3d
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-07-07 09:11:09 +0000

    update project done

--- controllers/projectsController.ts
+++ controllers/projectsController.ts
@@ -9,22 +9,22 @@ const getAllProjects = async (req: Request, res: Response) => {
 };
 
 const getProject = async (req: Request, res: Response) => {
-  const userId =(req as CustomExpressRequest).user.userId
+  const userId = (req as CustomExpressRequest).user.userId;
   const { id } = req.params;
   const project = await prisma.project.findMany({
     where: {
       id: Number(id),
-      creatorId: Number(userId)  
-    }
+      creatorId: Number(userId),
+    },
   });
-  
+
   if (project.length < 1) {
-    res
+    return res
       .status(StatusCodes.NOT_FOUND)
       .json({ msg: `No project found with id: ${id}` });
   }
   if (!project) {
-    res
+    return res
       .status(StatusCodes.NOT_FOUND)
       .json({ msg: `No project found with id: ${id}` });
   }
@@ -35,7 +35,6 @@ const createProject = async (req: Request, res: Response) => {
   const createdBy = {
     connect: { id: Number((req as CustomExpressRequest).user.userId) },
   };
-  console.log(createdBy)
   req.body.createdby = createdBy;
   const job = await prisma.project.create({
     data: req.body,
@@ -43,4 +42,49 @@ const createProject = async (req: Request, res: Response) => {
   res.status(StatusCodes.CREATED).json({ data: job });
 };
 
-export { createProject, getAllProjects, getProject };
+const updateProject = async (req: Request, res: Response) => {
+  const userId = (req as CustomExpressRequest).user.userId;
+  const { id } = req.params;
+  const findProject = await prisma.project.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      createdby: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+
+  if (!findProject) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `This project does not exist` });
+  }
+
+  if (findProject?.createdby.id !== Number(userId)) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `You're not eligible to update this project` });
+  }
+
+  const project = await prisma.project.update({
+    where: {
+      id: Number(id),
+      // creatorId: Number(userId),
+    },
+    data: req.body,
+  });
+
+  if (project) {
+    return res.status(StatusCodes.OK).json({ data: project });
+  }
+
+  res
+    .status(StatusCodes.NOT_FOUND)
+    .json({ msg: `No project found with id: ${id}` });
+};
+
+export { createProject, getAllProjects, getProject, updateProject };
--- routes/projectRoutes.ts
+++ routes/projectRoutes.ts
@@ -5,11 +5,12 @@ import {
   createProject,
   getAllProjects,
   getProject,
+  updateProject,
 } from "../controllers/projectsController";
 import autauthenticationMiddleware from "../middleware/authentication";
 
 router.use(autauthenticationMiddleware);
 
 router.route("/").post(createProject).get(getAllProjects);
-router.route("/:id").get(getProject);
+router.route("/:id").get(getProject).patch(updateProject)
 export default router;
